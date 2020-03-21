fabcc1588df3b676f8955731bdbb0a8d147c21ad
Ana Fraire <afg_imv@hotmail.com>
2020-03-21 23:42:59 +0100

    video 14

--- core/src/com/anafraire/mario/Estados/EstadoJuego.java
+++ core/src/com/anafraire/mario/Estados/EstadoJuego.java
@@ -1,17 +1,18 @@
 package com.anafraire.mario.Estados;
 
 import com.anafraire.mario.Mario;
+import com.anafraire.mario.Objetos.Pajaro;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class EstadoJuego extends Estado{
 
-    private Texture pajaro;
+    private Pajaro pajaro;
 
     public EstadoJuego(AdminEstadosJuego gsm) {
         super(gsm);
-        pajaro = new Texture("bird.png");
+        pajaro = new Pajaro(50, 200);
         camara.setToOrtho(false, Mario.ANCHURA/2, Mario.ALTURA/2);
     }
 
@@ -22,14 +23,15 @@ public class EstadoJuego extends Estado{
 
     @Override
     public void update(float deltaTime) {
-
+        handleInput();
+        pajaro.update(deltaTime);
     }
 
     @Override
     public void render(SpriteBatch spriteBatch) {
         spriteBatch.setProjectionMatrix(camara.combined);
         spriteBatch.begin();
-        spriteBatch.draw(pajaro, 50, 50);
+        spriteBatch.draw(pajaro.getPajaro(), pajaro.getPosicion().x, pajaro.getPosicion().y);
         spriteBatch.end();
     }
 
