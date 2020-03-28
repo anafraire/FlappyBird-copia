a12c8f82a72eb71d3d4f0d0b94d106af341e16f3
Ana Fraire <afg_imv@hotmail.com>
2020-03-28 22:51:10 +0100

    video 20

--- core/src/com/anafraire/mario/Estados/EstadoJuego_Escena.java
+++ core/src/com/anafraire/mario/Estados/EstadoJuego_Escena.java
@@ -12,7 +12,7 @@ public class EstadoJuego_Escena extends Estado{
 
     private Pajaro pajaro;
     private Texture fondo2;
-    private Tuberia tuberia;
+    //private Tuberia tuberia;
     private static final int ESPACIO_TUBERIAS = 125;
     private static final int CONTADOR_TUBERIAS = 5;
     private Array <Tuberia> tuberias;
@@ -41,12 +41,16 @@ public class EstadoJuego_Escena extends Estado{
         handleInput();
         pajaro.update(deltaTime);
 
+        camara.position.x = pajaro.getPosicion().x + 80;    //camara del juego
+
         for(Tuberia tuberia : tuberias){
             //Posicion del tubo de arriba
             if(camara.position.x - (camara.viewportHeight/2) > tuberia.getPosicionTopTuberia().x + tuberia.getTopTuberia().getWidth()){
                 tuberia.reposicion(tuberia.getPosicionTopTuberia().x + (Tuberia.ANCHURA_TUBERIA + ESPACIO_TUBERIAS) * CONTADOR_TUBERIAS);
             }
         }
+
+        camara.update();
     }
 
     @Override
@@ -56,8 +60,10 @@ public class EstadoJuego_Escena extends Estado{
         //pantalla cuando está el pájaro
         spriteBatch.draw(fondo2, camara.position.x- (camara.viewportWidth/2), camara.position.y- (camara.viewportHeight/2));
         spriteBatch.draw(pajaro.getPajaro(), pajaro.getPosicion().x, pajaro.getPosicion().y);
-        spriteBatch.draw(tuberia.getTopTuberia(), tuberia.getPosicionTopTuberia().x, tuberia.getPosicionTopTuberia().y);//Tuberia superior
-        spriteBatch.draw(tuberia.getDownTuberia(), tuberia.getPosicionDownTuberia().x, tuberia.getPosicionDownTuberia().y);//Tuberia inferior
+        for (Tuberia tuberia : tuberias){
+            spriteBatch.draw(tuberia.getTopTuberia(), tuberia.getPosicionTopTuberia().x, tuberia.getPosicionTopTuberia().y);//Tuberia superior
+            spriteBatch.draw(tuberia.getDownTuberia(), tuberia.getPosicionDownTuberia().x, tuberia.getPosicionDownTuberia().y);//Tuberia inferior
+        }
         spriteBatch.end();
     }
 
