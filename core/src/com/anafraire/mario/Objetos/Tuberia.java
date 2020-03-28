23ca645b46b94f7964e96e09ac17434c5e631bf4
Ana Fraire <afg_imv@hotmail.com>
2020-03-28 23:51:33 +0100

    video 21/30

--- core/src/com/anafraire/mario/Objetos/Tuberia.java
+++ core/src/com/anafraire/mario/Objetos/Tuberia.java
@@ -1,6 +1,7 @@
 package com.anafraire.mario.Objetos;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import java.util.Random;
 
@@ -15,6 +16,8 @@ public class Tuberia {
     private static final int VARIACION = 130;   //Rango entre el que se mueven las tuberias
     private static final int TUBERIA_GAP = 100;   //Distancia entre un tubo y otro
     private static final int INICIO_BAJO = 0;   //No bajar demasiado la tuberia
+    private Rectangle limiteSuperior;
+    private Rectangle limiteInferior;
 
     public static final int ANCHURA_TUBERIA = 52;
 
@@ -26,12 +29,29 @@ public class Tuberia {
 
         posicionTopTuberia = new Vector2(x, random.nextInt(VARIACION) + TUBERIA_GAP + INICIO_BAJO);
         posicionDownTuberia = new Vector2(x, posicionTopTuberia.y - TUBERIA_GAP - downTuberia.getHeight());
+
+        limiteSuperior = new Rectangle(posicionTopTuberia.x, posicionTopTuberia.y,
+                topTuberia.getWidth(), topTuberia.getHeight());  //contorno tubo superior
+        limiteInferior  = new Rectangle(posicionDownTuberia.x, posicionDownTuberia.y,
+                downTuberia.getWidth(), downTuberia.getHeight());   //contorno tubo inferior
     }
 
     public void reposicion (float x){   //Reposicionar las tuberias
         //Posicion random en y
         posicionTopTuberia.set(x, random.nextInt(VARIACION) + TUBERIA_GAP + INICIO_BAJO);
         posicionDownTuberia.set(x, posicionTopTuberia.y - TUBERIA_GAP - downTuberia.getHeight());//Alinear tuberia inferior con superior
+
+        limiteSuperior.setPosition(posicionTopTuberia.x, posicionTopTuberia.y); //contorno de los tubos a la derecha top
+        limiteInferior.setPosition(posicionDownTuberia.x, posicionDownTuberia.y); //contorno de los tubos a la derecha down
+    }
+
+    public boolean colision (Rectangle jugador) {
+        boolean res = false;
+        if(jugador.overlaps(limiteSuperior) || jugador.overlaps(limiteInferior)){
+            res = true;
+        }
+        return res;
+        //return jugador.overlaps(limiteSuperior) || jugador.overlaps(limiteInferior);  //tocar una tuberia
     }
 
     public Texture getTopTuberia() {
